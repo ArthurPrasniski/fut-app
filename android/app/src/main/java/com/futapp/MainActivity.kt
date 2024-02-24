0f5c58b8adf0cbeeb10075cc9adc49e57ae7bab9
Arthur Prasniski <prasniski@outlook.com>
2024-02-24 01:01:43 -0300

    fix: fixed name com.

--- android/app/src/main/java/com/futapp/MainActivity.kt
+++ android/app/src/main/java/com/futapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.futapp
+package com.futappbyapv
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/futapp/MainApplication.kt
+++ android/app/src/main/java/com/futapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.futapp
+package com.futappbyapv
 
 import android.app.Application
 import android.content.res.Configuration
